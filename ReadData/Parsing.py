a0a79d3a868a0cbbd4aec23c30f3e0a6a1fe71b6
sehwa <nsh235482@gmail.com>
2018-05-03 22:44:16 +0900

    개봉 후 평점 정보 파싱 추가

--- ReadData/Parsing.py
+++ ReadData/Parsing.py
@@ -16,6 +16,7 @@ def agency():
         agency_name = driver.find_element_by_xpath("//dl[@class='agency_name']")
         agency_text = agency_name.text
         
+        #제작사 / 수입사 / 배급사 정보 존재 유무 확인 (없으면 None)
         try:
             producer_test = driver.find_elements_by_class_name("agency_sub0")
             if producer_test == [] :
@@ -40,6 +41,7 @@ def agency():
             print("배급사 없음")
             distributor_test = None
             
+        #제작사 / 수입사 / 배급사 데이터 저장
         if producer_test is not None and importer_test is not None and distributor_test is not None :
             producer = agency_text.split('\n')[0]
             importer = agency_text.split('\n')[1]
@@ -90,9 +92,52 @@ def beforeOpeningscore():
         #네티즌 평점
         star_score_text = driver.find_element_by_id("beforePointArea")
         star_score = star_score_text.text.split('\n')[3]
-        participator = star_score_text.text.split('\n')[4]
+        before_participator = star_score_text.text.split('\n')[4]
         print("개봉 전 평점 : " + star_score)
-        print("참여자 수 : " + participator.replace("참여", ""))
+        print("참여자 수 : " + before_participator.replace("참여", ""))
     except:
         print("국내 개봉 되지 않은 영화입니다")
-    
+        
+#[평점] 개봉 후 평점 정보 파싱
+def afterOpeningscore():
+    try:
+        #네티즌 평점 정보 (총 평점 - 별점)
+        netizen_score_all = driver.find_element_by_id("netizen_point_tab_inner")
+        netizen_score = netizen_score_all.text.split('\n')[0]
+        after_participator_netizen = netizen_score_all.text.split('\n')[1]
+        
+        print("네티즌 평점 : " + netizen_score)
+        print("네티즌 평점 참여자 수 : " + after_participator_netizen)
+    except:
+        print("평점 정보 없음")
+        
+    try:
+        #네티즌 점수 분포 (점수 분포 막대 그래프)
+        netizen_score_graph_text = driver.find_element_by_id("netizen_point_graph")
+        netizen_score_graph = netizen_score_graph_text.text.split("\n")
+        #점수 분포 리스트
+        netizen_score_graph_list = []
+        
+        #index가 2로 안나누어 떨어지는 netizen_score_graph 요소만 netizen_score_graph_list에 삽입
+        for index in range(0, len(netizen_score_graph)):
+            if index % 2 != 0 :
+                netizen_score_graph_list.append(netizen_score_graph[index])
+        
+        #이 영화를 선호하는 연령대 텍스트 전처리
+        netizen_score_favorite_group_text = netizen_score_graph_list[-1].replace("이 영화를 가장 좋아하는 그룹은 ", "")
+        netizen_score_favorite_group = netizen_score_favorite_group_text.replace("입니다.", "")    
+        
+        #리스트 첫 번째 요소 삭제 (필요없는 데이터이므로 삭제)
+        del netizen_score_graph_list[0]
+                
+        #리스트 마지막 요소 삭제(선호하는 연령대 데이터)
+        del netizen_score_graph_list[-1]
+        
+        #점수 분포 텍스트 전처리 ('%' 삭제)
+        for index in range(0, len(netizen_score_graph_list)):
+            netizen_score_graph_list[index] = netizen_score_graph_list[index].replace("%", "")
+        
+        print(netizen_score_graph_list)
+        print("선호하는 그룹 : " +netizen_score_favorite_group)  
+    except:
+        print("그래프 정보 없음")
