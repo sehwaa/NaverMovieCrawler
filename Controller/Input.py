a22d716179544a54c453bea3d960b816d2128af7
sehwa <nsh235482@gmail.com>
2018-05-17 16:31:01 +0900

    자원 로딩 시간(3초) 추가

--- Controller/Input.py
+++ Controller/Input.py
@@ -10,6 +10,7 @@ from Connection.Connection import driver
 #영화 검색란에 영화 검색하기
 def inputMovieName(movieName):
     driver.find_element_by_id('ipt_tx_srch').send_keys(movieName)
+    driver.implicitly_wait(3)
     
 #영화 검색하는 칸 텍스트 클리어
 def clear():
